06ee9de9702219675be8e9b86a21e358d9e90d85
amarachiugwu <marycynthiamara2@gmail.com>
2022-09-22 14:36:44 +0100

    deployed to goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,29 +1,69 @@
+require("@nomiclabs/hardhat-waffle");
+// import "@nomiclabs/hardhat-ethers";
+// require("@nomiclabs/hardhat-etherscan")
+// require('dotenv').config();
+require("dotenv").config({ path: ".env" });
 
-/* global ethers task */
-require('@nomiclabs/hardhat-waffle')
+const ALCHEMY_RINKEBY_API_KEY_URL = process.env.ALCHEMY_RINKEBY_API_KEY_URL;
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async () => {
-  const accounts = await ethers.getSigners()
+const ALCHEMY_GOERLI_API_KEY_URL = process.env.ALCHEMY_GOERLI_API_KEY_URL;
 
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
+const INFURA_GOERLI_API_KEY_URL = process.env.INFURA_GOERLI_API_KEY_URL;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const INFURA_KOVAN_API_KEY_URL = process.env.INFURA_KOVAN_API_KEY_URL;
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const INFURA_ROPSTEN_API_KEY_URL = process.env.INFURA_ROPSTEN_API_KEY_URL;
+
+const ACCOUNT_PRIVATE_KEY1 = process.env.ACCOUNT_PRIVATE_KEY1;
+
+// const API_TOKEN = process.env.API_TOKEN;
+
 module.exports = {
-  solidity: '0.8.6',
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      forking: {
+        // url : INFURA_ROPSTEN_API_KEY_URL,
+        url: "https://mainnet.infura.io/v3/e92c38757159497d97aad034c8e59232",
+        // blockGasLimit: 200000000000,
+        // gasPrice: 10000000000,
+      }
+    },
+    rinkeby: {
+      url: ALCHEMY_RINKEBY_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY1],
+    },
+    ropsten: {
+      url: INFURA_ROPSTEN_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY1],
+    },
+    goerli: {
+      url: INFURA_GOERLI_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY1],
+    },
+    kovan: {
+      url: INFURA_KOVAN_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY1],
+    },
+    // mainnet: {
+    //   url: ALCHEMY_MAINNET_API_KEY_URL,
+    //   accounts: [ACCOUNT_PRIVATE_KEY],
+    // },
+    // mumbai: {
+    //   url: ALCHEMY_MUMBAI_API_KEY_URL,
+    //   accounts: [ACCOUNT_PRIVATE_KEY],
+    // }
+  },
+
+
+  // etherscan: {
+  //   apiKey: API_TOKEN
+  // }
+
   settings: {
     optimizer: {
       enabled: true,
       runs: 200
     }
   }
-}
+};
