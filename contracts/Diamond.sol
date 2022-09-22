a3fda102371087f2440a5a85a812864d0f28937c
amarachiugwu <marycynthiamara2@gmail.com>
2022-09-22 12:49:08 +0100

    modified /contracts Added ERC721 facet Added required ERC721 interfaces Created AppStorage Used String and Address Library Modified Diamond Constructor Modified DiamondInit body

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -8,12 +8,14 @@ pragma solidity ^0.8.0;
 * Implementation of a diamond.
 /******************************************************************************/
 
-import { LibDiamond } from "./libraries/LibDiamond.sol";
-import { IDiamondCut } from "./interfaces/IDiamondCut.sol";
+import {LibDiamond} from "./libraries/LibDiamond.sol";
+import {IDiamondCut} from "./interfaces/IDiamondCut.sol";
+import {AppStorage} from "./libraries/AppStorage.sol";
+contract Diamond {
 
-contract Diamond {    
-
-    constructor(address _contractOwner, address _diamondCutFacet) payable {        
+    AppStorage internal s;
+    
+    constructor(address _contractOwner, address _diamondCutFacet, string memory name, string memory symbol) payable {
         LibDiamond.setContractOwner(_contractOwner);
 
         // Add the diamondCut external function from the diamondCutFacet
@@ -21,11 +23,13 @@ contract Diamond {
         bytes4[] memory functionSelectors = new bytes4[](1);
         functionSelectors[0] = IDiamondCut.diamondCut.selector;
         cut[0] = IDiamondCut.FacetCut({
-            facetAddress: _diamondCutFacet, 
-            action: IDiamondCut.FacetCutAction.Add, 
+            facetAddress: _diamondCutFacet,
+            action: IDiamondCut.FacetCutAction.Add,
             functionSelectors: functionSelectors
         });
-        LibDiamond.diamondCut(cut, address(0), "");        
+        LibDiamond.diamondCut(cut, address(0), "");
+		s._name = name;
+		s._symbol = symbol;
     }
 
     // Find facet for function that is called and execute the
@@ -50,14 +54,15 @@ contract Diamond {
             returndatacopy(0, 0, returndatasize())
             // return any return value or error back to the caller
             switch result
-                case 0 {
-                    revert(0, returndatasize())
-                }
-                default {
-                    return(0, returndatasize())
-                }
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
         }
     }
+
 
     receive() external payable {}
 }
--- contracts/upgradeInitializers/DiamondInit.sol
+++ contracts/upgradeInitializers/DiamondInit.sol
@@ -13,6 +13,9 @@ import { IDiamondLoupe } from "../interfaces/IDiamondLoupe.sol";
 import { IDiamondCut } from "../interfaces/IDiamondCut.sol";
 import { IERC173 } from "../interfaces/IERC173.sol";
 import { IERC165 } from "../interfaces/IERC165.sol";
+import { IERC721} from "../interfaces/IERC721.sol";
+import { IERC721Receiver } from "../interfaces/IERC721Receiver.sol";
+
 
 // It is expected that this contract is customized if you want to deploy your diamond
 // with data from a deployment script. Use the init function to initialize state variables
@@ -29,6 +32,8 @@ contract DiamondInit {
         ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
         ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
         ds.supportedInterfaces[type(IERC173).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC721).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC721Receiver).interfaceId] = true;
 
         // add your own state variables 
         // EIP-2535 specifies that the `diamondCut` function takes two optional 
